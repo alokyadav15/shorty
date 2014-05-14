8abb059bcc4aaeed0cdcad1970e547f80b3921c8
Alok Yadav <alokyadav15@gmail.com>
2014-05-14 23:33:31 +0530

    email validation on users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,10 @@
-class EmailValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || "is not an email")
-    end
-  end
-end
- 
 
 class User < ActiveRecord::Base
 has_many :keys
 
 validates_presence_of :email , :on => :create
-validates :email, presence: true, email: true
+validates :email, presence: true 
+validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/
 validates_uniqueness_of :current_token
 # raise
 before_create :create_api_token
